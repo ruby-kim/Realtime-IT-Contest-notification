bbdc948b4ce85a7088dca2c75358df5d3afc132c
mike2ox <firefinger07@gmail.com>
2020-07-31 15:58:06 +0900

    To modify code
    - can't access table elements(L20)
    - don't remove chromedriver.exe(check ps)

--- srcs/test.py
+++ srcs/test.py
@@ -1,13 +1,23 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 import requests
 
-path = "https://thinkcontest.com"
+sitepath = "https://thinkcontest.com"
+chromepath = "./.lib/chromedriver.exe"
 contestTitles = []
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('lang=ko_KR')
+driver = webdriver.Chrome(chromepath, options=options)
+driver.implicitly_wait(3)
 
-req = requests.get(path)
-objs = BeautifulSoup(req.content, "html.parser").select(".contest-title a")
+driver.get(sitepath)
 
-for i in objs:
-	contestTitles += i.contents
+# for i in range(1, 10):
+tb = driver.find_element_by_class_name("type-2.mg-t-5.contest-table")
 
-print(contestTitles)
+for tr in tb.find_element_by_tag_name("tr"):
+	title = tr.find_element_by_tag_name('a')
+	print(title.text)
+driver.quit()
