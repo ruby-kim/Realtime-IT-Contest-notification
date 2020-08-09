ea37f3c5bf356249934e8cf4a533d4b27192af44
mike2ox <firefinger07@gmail.com>
2020-08-09 20:28:10 +0900

    Apply pandas dataframe

--- srcs/main.py
+++ srcs/main.py
@@ -1,5 +1,6 @@
 from thinkgood import Thinkgood
 from campusmon import Campusmon
+import pandas as pd
 
 if __name__ == '__main__':
     thinkgood = Thinkgood()
@@ -8,4 +9,10 @@ if __name__ == '__main__':
     thinkgood.crawling()
 
     print("===== thinkgood list =====")
-    thinkgood.check_result()
+    # thinkgood.check_result()
+
+    df = pd.DataFrame(thinkgood.infos).T
+    df.rename(columns={0 :'기간', 1 : '분야', 2 : '주최', 3 : '링크'}, inplace=True)
+
+    print(df)
+
