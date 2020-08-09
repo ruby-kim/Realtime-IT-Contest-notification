8ce611eec2dc79767f05ec4e06aa2500606729ef
mike2ox <firefinger07@gmail.com>
2020-08-09 19:43:02 +0900

    Update main code

--- srcs/main.py
+++ srcs/main.py
@@ -2,15 +2,10 @@ from thinkgood import Thinkgood
 from campusmon import Campusmon
 
 if __name__ == '__main__':
-    campusmon = Campusmon()
     thinkgood = Thinkgood()
 
-    print("===== campusmon crawling =====")
-    campusmon.crawling()
     print("===== thinkgood crawling =====")
     thinkgood.crawling()
 
-    print("===== campusmon list =====")
-    campusmon.check_result()
     print("===== thinkgood list =====")
     thinkgood.check_result()
